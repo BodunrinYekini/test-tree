0727c78c62c58cb2c006ea08d82896473dddd4ed
Bodunrin <yekinibodunrin@gmail.com>
2024-05-18 20:07:54 +0200

    aosp product remove

--- aosp_aleph.mk
+++ aosp_aleph.mk
@@ -7,7 +7,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
