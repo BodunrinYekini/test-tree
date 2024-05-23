835b2497fcf40761cc58357709661e322fd7129e
Bodunrin <yekinibodunrin@gmail.com>
2024-05-24 01:53:12 +0200

    try multiple files

--- packages/Android.mk
+++ packages/Android.mk
@@ -319,7 +319,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := LogManager
-LOCAL_SRC_FILES := packages/vendor/app/LogManager/LogManager.apk
+LOCAL_SRC_FILES := packages/vendor/app/LogManager/
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
