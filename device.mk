c9068e5b92bfa4715ab9f67a0b9549596d347cb7
Bodunrin <yekinibodunrin@gmail.com>
2024-05-26 01:20:09 +0200

    remove vendor app and overlay

--- device.mk
+++ device.mk
@@ -94,23 +94,7 @@ PRODUCT_PACKAGES += \
     unisoc-res__auto_generated_rro_product \
     WallpaperOverlay \
     com.google.mainline.go.telemetry \
-    apns-conf.xml \
-    AospBtOverlay \
-    AospWifiOverlay_Marlin3 \
-    AospWifiOverlay_Marlin3_Mainline \
-    UniWifiOverlay_Marlin3 \
-    MultiuserOverlays \
-    NetworkStackOverlayGo \
-    NetworkStackOverlayGsi \
-    Settings__auto_generated_rro_vendor \
-    TetheringConfigOverlayGo \
-    TetheringConfigOverlayGsi \
-    unisoc_go_overlay_frameworks_res \
-    unisoc_overlay_frameworks_res \
-    LogManager \
-    ProxyNFwLocation \
-    UASetting \
-    WirelessTools
+    apns-conf.xml
 
 PRODUCT_PACKAGES += \
     fstab.ums9230_1h10 \
