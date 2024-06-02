3133a39bfe045ff562eec4f66c9accff8e3c50e6
Bodunrin <yekinibodunrin@gmail.com>
2024-06-02 18:06:36 +0200

    remove aosp base

--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 
