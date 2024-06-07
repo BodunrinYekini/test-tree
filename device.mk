b8893705aa9a35ac7681b1b7b4b160e692d806b0
Bodunrin <yekinibodunrin@gmail.com>
2024-06-07 20:59:23 +0200

    go

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
