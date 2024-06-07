e200780453e72f9dd7740c6ae9b47365e8fc1015
Bodunrin <yekinibodunrin@gmail.com>
2024-06-07 21:35:06 +0200

    try go

--- device.mk
+++ device.mk
@@ -8,7 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 
 DEVICE_PATH := device/ohrtech/aleph
