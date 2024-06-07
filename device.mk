6904dec3a15dccdd9490a18d962b7f3d3eaf1522
Bodunrin <yekinibodunrin@gmail.com>
2024-06-07 13:57:44 +0200

    edit for system_ext

--- device.mk
+++ device.mk
@@ -9,8 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 
 DEVICE_PATH := device/ohrtech/aleph
