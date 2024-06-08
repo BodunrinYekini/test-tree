9103dc27b5fbd4fba48e017e54425d03469d0a49
Bodunrin <yekinibodunrin@gmail.com>
2024-06-08 18:46:37 +0200

    system and product only

--- device.mk
+++ device.mk
@@ -13,15 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 
-#system_ext
-PRODUCT_PACKAGES += \
-    fs_config_dirs_system_ext \
-    fs_config_files_system_ext \
-    group_system_ext \
-    passwd_system_ext \
-    selinux_policy_system_ext \
-    system_ext_manifest.xml \
-
 
 
 
