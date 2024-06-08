1cf45a881766836f33c2d4c593f9f6fbf70a07ed
Bodunrin <yekinibodunrin@gmail.com>
2024-06-08 18:06:26 +0200

    system-ext

--- device.mk
+++ device.mk
@@ -10,10 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 
+#system_ext
 PRODUCT_PACKAGES += \
     fs_config_dirs_system_ext \
     fs_config_files_system_ext \
