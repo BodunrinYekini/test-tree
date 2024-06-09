cf0ce0da1e7a9feaf2df2935f95009691601e65d
Bodunrin <yekinibodunrin@gmail.com>
2024-06-09 18:50:57 +0100

    system-ext priv

--- device.mk
+++ device.mk
@@ -62,7 +62,22 @@ $(call find-copy-subdir-files,*,$(LOCAL_PATH)/modules/system_ext/lib/,$(TARGET_C
 $(call find-copy-subdir-files,*,$(LOCAL_PATH)/modules/system_ext/lib64/,$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64)
 
 PRODUCT_PACKAGES += \
-AIEngineService
+AIEngineService \
+ims \
+NetworkSliceAgent \
+NewMusic \
+OmacpPrebuilt \
+QuickCamera \
+radio_interactor_service \
+ServiceUniWifiResources \
+SprdCalendarProviderPrebuilt \
+SprdContacts \
+SprdContactsProvider \
+SprdDialerGo \
+Srmi \
+UniSilentReboot \
+UniTelephony \
+UniWifiDialog
 
 
 #PRODUCT_PACKAGES += \
