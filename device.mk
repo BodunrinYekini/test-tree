5662f7b7134e1b2a20889f710e02a0ac98ec2a7b
Bodunrin <yekinibodunrin@gmail.com>
2024-06-09 09:16:38 +0200

    64 bit

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
