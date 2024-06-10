76607a098fcf666108410f26b0d1a293ed25c41c
Bodunrin <yekinibodunrin@gmail.com>
2024-06-10 09:47:10 +0100

    try

--- device.mk
+++ device.mk
@@ -51,47 +51,7 @@ WallpaperCropper \
 ThemePicker \
 RemoteProvisioner \
 CarrierConfig \
-EmergencyInfo \
-CameraCalibration \
-CameraCalibrationOdex \
-CameraCalibrationVdex \
-AIEngineService \
-ims \
-NetworkSliceAgent \
-NewMusic \
-OmacpPrebuilt \
-QuickCamera \
-radio_interactor_service \
-ServiceUniWifiResources \
-SprdCalendarProviderPrebuilt \
-SprdContacts \
-SprdContactsProvider \
-SprdDialerGo \
-Srmi \
-UniSilentReboot \
-UniTelephony \
-UniWifiDialog \
-CameraCalibration \
-CamTa \
-DreamCamera2 \
-DreamFMRadioPrebuilt \
-DreamSoundRecorderPrebuilt \
-EngineerMode \
-LinkTurbo \
-NewGallery2_prebuilt \
-SGPS \
-SoterService \
-SprdCommLogService \
-UnisocStk \
-USCPhotosProvider \
-ValidationTools \
-com.unisoc.sdk.common \
-smartlink_sdk \
-unipnp-framework \
-unisoc-framework \
-uni-telephony-common \
-unisoc-res
-    
+EmergencyInfo
 
 PRODUCT_COPY_FILES += \
 $(call find-copy-subdir-files,*,$(LOCAL_PATH)/modules/system_ext/bin/,$(TARGET_COPY_OUT_SYSTEM_EXT)/bin) \
--- modules/Android.mk
+++ modules/Android.mk
@@ -1,35 +1,10 @@
 include $(CLEAR_VARS)
-LOCAL_MODULE := CameraCalibration
-LOCAL_MODULE_OWNER := ohrtech
-LOCAL_SRC_FILES := modules/system_ext/app/CameraCalibration/CameraCalibration.apk
-LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := unisoc-res
+LOCAL_SRC_FILES := modules/system_ext/framework/unisoc-res.apk
 LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := CameraCalibrationOdex
-LOCAL_MODULE_OWNER := ohrtech
-LOCAL_SRC_FILES := modules/system_ext/app/CameraCalibration/oat/arm64/CameraCalibration.odex
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .odex
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := CameraCalibrationVdex
-LOCAL_MODULE_OWNER := ohrtech
-LOCAL_SRC_FILES := modules/system_ext/app/CameraCalibration/oat/arm64/CameraCalibration.vdex
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .vdex
-LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT)/framework
+LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 include $(BUILD_PREBUILT)
