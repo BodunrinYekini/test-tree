4574d852ca60fe1ccf1758389052562d640d2b66
Bodunrin <yekinibodunrin@gmail.com>
2024-06-12 14:33:09 +0100

    save

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),aleph)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
