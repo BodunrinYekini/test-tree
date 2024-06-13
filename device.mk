53fdc42339442afe73976b6a9cfa3fe303eab217
Bodunrin <yekinibodunrin@gmail.com>
2024-06-13 11:10:06 +0100

    last

--- device.mk
+++ device.mk
@@ -18,9 +18,7 @@ DEVICE_PATH := device/ohrtech/aleph
 #product
 PRODUCT_PACKAGES += \
     WallpaperPicker \
-    apns-conf.xml \
-    framework-res__auto_generated_rro_product \
-    FrameworkResOverlay
+    apns-conf.xml
 
 # PRODUCT_PACKAGES += \
 #     vndservicemanager \
