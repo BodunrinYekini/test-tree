f27ed6a97329e02845715ccaf35b2fe14005128e
Bodunrin <yekinibodunrin@gmail.com>
2024-06-13 22:01:21 +0100

    gsi release

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system_arm64.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system_arm64.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_release.mk)
 
 
