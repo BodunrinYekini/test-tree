b4cf4f7b4865764b4ac60f1b0dff99060581053f
Bodunrin <yekinibodunrin@gmail.com>
2024-06-23 15:40:54 +0100

    matrixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,25 +212,25 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 
 # VINTF
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
 #/vendor/etc/vintf/compatibility_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/vendor_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vendor_compatibility_matrix.xml
 
 #/odm/etc/vintf/manifest.xml
-ODM_MANIFEST_FILES += $(DEVICE_PATH)/odm_manifest.xml
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/odm_manifest.xml
 
 #/system_ext/etc/vintf/manifest.xml
-SYSTEM_EXT_MANIFEST_FILES += $(DEVICE_PATH)/system_ext_manifest.xml
+SYSTEM_EXT_MANIFEST_FILES := $(DEVICE_PATH)/system_ext_manifest.xml
 
 #/product/etc/vintf/compatibility_matrix.xml 
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE+= $(DEVICE_PATH)/product_compatibility_matrix.xml 
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/product_compatibility_matrix.xml 
 
 #/system/etc/vintf/compatibility_matrix.device.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.device.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.device.xml
 
 #system/etc/vintf/manifest.xml	
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/system_framework_manifext.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/system_framework_manifest.xml
 
 # Inherit the proprietary files
 #include vendor/ohrtech/aleph/BoardConfigVendor.mk
