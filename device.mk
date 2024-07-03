1cf24c971cd988ffa54dfb836a4c7be3619125cd
Bodunrin <yekinibodunrin@gmail.com>
2024-07-03 21:12:51 +0100

    generic ramdisk

--- device.mk
+++ device.mk
@@ -7,6 +7,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
