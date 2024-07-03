b96efc4beecb44b317fbf2d85fc12513c070f441
Bodunrin <yekinibodunrin@gmail.com>
2024-07-03 16:55:28 +0100

    manifests

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 
 # 	/vendor/etc/vintf/manifest.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifests/manifest.xml
 
 # #/vendor/etc/vintf/compatibility_matrix.xml
 # DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vendor_compatibility_matrix.xml
@@ -225,7 +225,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 # SYSTEM_EXT_MANIFEST_FILES := $(DEVICE_PATH)/system_ext_manifest.xml
 
 # #/product/etc/vintf/compatibility_matrix.xml 
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/product_compatibility_matrix.xml 
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/manifests/product_compatibility_matrix.xml 
 
 # #/system/etc/vintf/compatibility_matrix.device.xml
 # DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.device.xml
