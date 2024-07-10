2ea8a8e3176492c8ef089f4b334fe96462fbb227
Bodunrin <yekinibodunrin@gmail.com>
2024-07-10 19:41:05 +0100

    odm manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,21 +233,21 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 
 
-# 	/vendor/etc/vintf/manifest.xml
+# /vendor/etc/vintf/manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifests/manifest.xml
 
-# #/product/etc/vintf/compatibility_matrix.xml 
+#/product/etc/vintf/compatibility_matrix.xml 
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/manifests/product_compatibility_matrix.xml 
 
+#/odm/etc/vintf/manifest.xml
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/odm_manifest.xml
 
 
-
-# #/vendor/etc/vintf/compatibility_matrix.xml
+#/vendor/etc/vintf/compatibility_matrix.xml
 # DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vendor_compatibility_matrix.xml
 
 
-# #/odm/etc/vintf/manifest.xml
-# ODM_MANIFEST_FILES := $(DEVICE_PATH)/odm_manifest.xml
+
 
 # #/system_ext/etc/vintf/manifest.xml
 # SYSTEM_EXT_MANIFEST_FILES := $(DEVICE_PATH)/system_ext_manifest.xml
