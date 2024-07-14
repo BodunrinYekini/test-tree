632d64906059cf96ba2cb3fa927043d6a87805f8
Bodunrin <yekinibodunrin@gmail.com>
2024-07-14 16:37:34 +0100

    bare vendor

--- device.mk
+++ device.mk
@@ -19,25 +19,6 @@ TARGET_PREBUILT_KERNEL :=$(DEVICE_PATH)/prebuilts/kernel
 PRODUCT_COPY_FILES += \
 	$(TARGET_PREBUILT_KERNEL):kernel
 
-PRODUCT_PACKAGES += \
-    fstab.cali \
-    init.cali.rc \
-    init.ram.gms.rc \
-    init.ram.native.rc \
-    init.ram.rc \
-    init.storage.rc \
-    init.ums9230_1h10.rc \
-    init.ums9230_1h10.usb.rc \
-    init.ums9230_1h10_go.rc \
-    init.ums9230_1h10_go.usb.rc \
-    init.ums9230_4h10.rc \
-    init.ums9230_4h10.usb.rc \
-    init.ums9230_4h10_go.rc \
-    init.ums9230_4h10_go.usb.rc \
-    init.ums9230_4h10_go_AG801.rc \
-    init.ums9230_4h10_go_AG801.usb.rc \
-    init.zramwb.rc
-	
 
 #PRODUCT_COPY_FILES += \
 $(call find-copy-subdir-files,*,$(LOCAL_PATH)/manifests/vendor/manifest/,$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest) 
@@ -45,8 +26,9 @@ $(call find-copy-subdir-files,*,$(LOCAL_PATH)/manifests/vendor/manifest/,$(TARGE
 #copy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifests/vendor_compatibility_matrix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/compatibility_matrix.xml \
-	$(LOCAL_PATH)/rootdir/etc/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/lib/modules/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) 
+	$(LOCAL_PATH)/rootdir/etc/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
+
+#$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/lib/modules/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) 
 	
 
 
